bf69b0723caa03359ed35b4f6860ee71a862b0a1
Dan Le <tle57@gmu.edu>
2025-03-01 18:31:19 -0500

    add active styling for sidebar routes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,8 @@ import { History } from "lucide-react";
 import { NavLink } from "react-router";
 
 export default function Sidebar() {
+	const defSidebarStyle =
+		"flex items-center gap-3 rounded-md p-2 hover-css hover:bg-accent text-xl font-light";
 	return (
 		<div className="shrink-0 w-50 h-full bg-primary py-10 px-4 flex justify-center flex-col gap-25">
 			<span className="w-full flex flex-col gap-10 text-center text-3xl">
@@ -17,28 +19,44 @@ export default function Sidebar() {
 			<div className="h-full flex flex-col gap-4">
 				<NavLink
 					to={"profile"}
-					className="flex items-center gap-3 rounded-md p-2 hover-css hover:bg-accent text-xl font-light"
+					className={({ isActive }) =>
+						isActive
+							? "bg-accent " + defSidebarStyle
+							: defSidebarStyle
+					}
 				>
 					<UserRound size={25} />
 					Profile
 				</NavLink>
 				<NavLink
 					to={"/"}
-					className="flex items-center gap-3 rounded-md p-2 hover-css hover:bg-accent text-xl font-light"
+					className={({ isActive }) =>
+						isActive
+							? "bg-accent " + defSidebarStyle
+							: defSidebarStyle
+					}
 				>
 					<House size={25} />
 					Dashboard
 				</NavLink>
 				<NavLink
 					to={"workout"}
-					className="flex items-center gap-3 rounded-md p-2 hover-css hover:bg-accent text-xl font-light"
+					className={({ isActive }) =>
+						isActive
+							? "bg-accent " + defSidebarStyle
+							: defSidebarStyle
+					}
 				>
 					<Dumbbell size={25} />
 					Workouts
 				</NavLink>
 				<NavLink
 					to={"history"}
-					className="flex items-center gap-3 rounded-md p-2 hover-css hover:bg-accent text-xl font-light"
+					className={({ isActive }) =>
+						isActive
+							? "bg-accent " + defSidebarStyle
+							: defSidebarStyle
+					}
 				>
 					<History size={25} />
 					History
