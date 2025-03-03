5f6abdb5a8f75544b1f6c5c81e911dd6d14e9ad6
Shaun Jones <jonesshaun2021@gmail.com>
2025-03-03 13:29:41 -0500

    Home button on logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,13 +9,16 @@ export default function Sidebar() {
 		"flex items-center gap-3 rounded-md p-2 hover-css hover:bg-accent text-xl font-light";
 	return (
 		<div className="shrink-0 w-50 h-full bg-primary pt-10 pb-5 px-4 flex justify-center flex-col gap-25">
-			<span className="w-full flex flex-col gap-10 text-center text-3xl">
-				<div className="flex flex-col items-center gap-2 font-bold">
+			<NavLink
+				to={"dashboard"}
+				className="w-full flex flex-col gap-10 text-center text-3xl"
+			>
+				<div className="flex flex-col items-center gap-2 font-bold hover-css hover:bg-accent rounded-md p-2">
 					<BookMarked size={50} />
 					JymLog
 				</div>
 				<div className="w-full h-[2px] bg-snow-white opacity-80 rounded-xl"></div>
-			</span>
+			</NavLink>
 			<div className="h-full flex flex-col gap-4">
 				<NavLink
 					to={"profile"}
